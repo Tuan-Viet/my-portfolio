e0df84df0f00ca99dc75751b33499831d892aa98
TuanViet <vietlt226688@gmail.com>
2023-02-17 21:20:02 +0700

    init layout

--- src/pages/admin/ProjectsAdmin.js
+++ src/pages/admin/ProjectsAdmin.js
@@ -1,21 +1,29 @@
 
 const ProjectsAdmin = () => {
     return `
-    <table class="table">
-        <thead>
-        <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Dsc</th>
-            <th><button class="btn btn-success">Add</button></th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td></td>
-        </tr>
-        </tbody>
-    </table>
+    <table class="table table-dark">
+    <thead class="thead-dark">
+      <tr>
+        <th>#</th>
+        <th>Name</th>
+        <th>cate</th>
+        <th>date</th>
+        <th>dsc</th>
+        <th colspan="2"><button class="btn btn-success"><a href="">ADD</a></button></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td widht="75"><button class="btn btn-danger">REMOVE</button></td>
+        <td widht="75"><button class="btn btn-warning"><a href="">EDIT</a></button></td>
+      </tr>
+    </tbody>
+  </table>
   `
 }
 
